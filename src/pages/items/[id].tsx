d3e6f47a435289ac78a742f6b0b2c3c739a1568a
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2022-08-22 10:28:58 +0900

    8/22追記しました

--- src/pages/items/[id].tsx
+++ src/pages/items/[id].tsx
@@ -1,90 +1,17 @@
-// import { useRouter } from 'next/router';
-
-// const ProductDetail = () => {
-//   const router = useRouter();
-
-//   return (
-//     <div>
-//       <h2>詳細詳細詳細</h2>
-//     </div>
-//   );
-// };
-
-// export default ProductDetail;
 import Head from 'next/head';
 import React from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 
-// パスの構成要素を表す型 (/books/[id].tsx の id 部分など)
-// (Note) Params という名前にすると曖昧なので、アクセス時のパスから抽出する
-// 情報だということを示すために PathParams という名前にしています。
-type PathParams = {
-  id: string;
-};
+const About = () => {
+  const router = useRouter();
 
-// ページコンポーネントに渡される props の型
-// (Note) ページコンポーネント用の props であることを意識するために、
-// 一般的な Props ではなく、PageProps という名前にしています。
-type PageProps = {
-  title: string;
-  author: string;
-};
+  const { id } = router.query;
 
-// 事前生成するページのパス（URL のパラメータ部分）のリストを返します。
-// eslint-disable-next-line @typescript-eslint/require-await
-export const getStaticPaths: GetStaticPaths<
-  PathParams
-> = async () => {
-  // /books/001、/books/002、/books/003 のページを事前生成するには、
-  // 次のように paths プロパティの値を設定して返します。
-  // 本来は id のリストを外部 API（getBookList など）で取得します。
-  return {
-    paths: [
-      { params: { id: '1' } },
-      { params: { id: '002' } },
-      { params: { id: '003' } },
-    ],
-    fallback: false, // 上記以外のパスでアクセスした場合は 404 ページにする
-  };
-};
-
-// URL のパラメータ情報（プレースホルダー部分に指定された値）をもとに、
-// ページコンポーネントに渡す props データを生成します。
-// context.params プロパティでこれらのパラメータを参照できるので、
-// その値に応じて props データを生成して返すように実装します。
-// eslint-disable-next-line @typescript-eslint/require-await
-export const getStaticProps: GetStaticProps<PageProps> = async (
-  context
-) => {
-  // ファイル名が [id].tsx なので id パラメーターを取り出すことができる
-  const { id } = context.params as PathParams;
-
-  // 本来はここで getBook(id) のように API を呼び出してデータを取得する
-  const props: PageProps = {
-    title: `Title-${id}`,
-    author: `Author-${id}`,
-  };
-
-  // ページコンポーネントに渡す PageProps オブジェクトを
-  // props プロパティに設定したオブジェクトを返す
-  return { props };
-};
-
-// ページコンポーネントの実装
-const BookPage: React.FC<PageProps> = ({
-  title,
-  author,
-}: PageProps) => {
   return (
-    <>
-      <Head>
-        <title>{title} の詳細ページ</title>
-      </Head>
-      <ul>
-        <li>タイトル: {title}</li>
-        <li>著者: {author}</li>
-      </ul>
-    </>
+    <div>
+      <h1>商品詳細ページ</h1>
+    </div>
   );
 };
-export default BookPage;
+export default About;
--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
-import path from 'path';
-import fs from 'fs/promises';
+import Link from 'next/link';
+// import path from 'path';
+// import fs from 'fs/promises';
 
 type Props = {
   data: { id: number; name: string }[];
@@ -11,39 +12,49 @@ const PortfolioPosts = (props: Props) => {
   return (
     <div>
       <h1>商品一覧</h1>
-      <ul>
-        {data.map((v) => (
-          <>
-            <p>{v.id}</p>
-            <p>{v.name}</p>
 
-            <button>詳細はこちら</button>
-            <button>削除する</button>
-          </>
-        ))}
-      </ul>
+      {data.map((v) => (
+        <>
+          <p>{v.id}</p>
+          <p>
+            <Link href={`/items/${v.id}`}>
+              <a>{v.name}</a>
+            </Link>
+          </p>
+          <button>削除する</button>
+        </>
+      ))}
     </div>
   );
 };
 
 export default PortfolioPosts;
 
-export const getStaticProps: GetStaticProps = async () => {
-  // const data = [
-  //   {
-  //     id: '1',
-  //     description: 'いちご',
-  //   },
-  //   {
-  //     id: '2',
-  //     description: 'りんご',
-  //   },
-  // ];
+// export const getStaticProps: GetStaticProps = async () => {
+// const data = [
+//   {
+//     id: '1',
+//     description: 'いちご',
+//   },
+//   {
+//     id: '2',
+//     description: 'りんご',
+//   },
+// ];
 
-  const filePath = path.join(process.cwd(), 'data', 'db.json');
-  const json = await fs.readFile(filePath);
-  const parsedJson = JSON.parse(json.toString());
+//   const filePath = path.join(process.cwd(), 'data', 'db.json');
+//   const json = await fs.readFile(filePath);
+//   const parsedJson = JSON.parse(json.toString());
+//   return {
+//     props: { data: parsedJson.items },
+//   };
+// };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('http://localhost:8000/items');
+  const users = await res.json();
+
   return {
-    props: { data: parsedJson.items },
+    props: { data: users },
   };
 };
